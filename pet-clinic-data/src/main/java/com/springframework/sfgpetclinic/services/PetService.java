79de0013157ef3a1b84bf67cb135e7821e812c92
SosukeAizen833 <Spandan833@gmai.com>
2023-10-22 23:17:29 +0530

    Refactor services

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/PetService.java
@@ -5,14 +5,10 @@ import com.springframework.sfgpetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet,Long> {
+
     Pet findByLastName(String lastName);
 
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
 
 
