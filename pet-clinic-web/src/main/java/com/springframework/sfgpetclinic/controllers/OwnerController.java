c9dbcf043d602be008e7928c11d324b1537a3199
SosukeAizen833 <Spandan833@gmai.com>
2023-10-24 14:23:12 +0530

    Created owner controller and template

---
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package com.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"owners/","owners","owners/index","owners/index.html"})
+    public String getOwners(){
+        return "owners/index.html";
+    }
+}
