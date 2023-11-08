bd566c1dfdd30cf4f5a166583bb66f08ea9d50e4
SosukeAizen833 <Spandan833@gmai.com>
2023-11-08 22:51:32 +0530

    Modified vet save method

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,14 +1,19 @@
 package com.springframework.sfgpetclinic.services.map;
 
+import com.springframework.sfgpetclinic.model.Speciality;
 import com.springframework.sfgpetclinic.model.Vet;
 import com.springframework.sfgpetclinic.services.CrudService;
+import com.springframework.sfgpetclinic.services.SpecialityService;
 import com.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 @Service
 public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService{
-
+    private  final SpecialityService specialityService;
+    public  VetServiceMap(SpecialityMapService specialityMapService){
+        this.specialityService = specialityMapService;
+    }
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -21,6 +26,14 @@ public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetSe
 
     @Override
     public Vet save(Vet object) {
+        if(object.getSpecialities().size() > 0){
+            object.getSpecialities().forEach(speciality -> {
+                if(speciality.getId() == null){
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
