561326f37e34a7561cd81d8c24a1a19d837735ec
SosukeAizen833 <Spandan833@gmai.com>
2024-01-13 18:36:11 +0530

    Fixed failing test

--- pet-clinic-web/src/test/java/com/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -75,7 +75,7 @@ class OwnerControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/owners/find"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/findOwners"))
-                .andExpect(model().attributeExists("owners"));
+                .andExpect(model().attributeExists("owner"));
 
         Mockito.verifyNoInteractions(ownerService);
     }
